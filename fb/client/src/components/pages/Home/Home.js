bbdf6d5ea199d5ceb9d7b3ffbd480b7cb6bce8f0
Chlora <ma662@njit.edu>
2019-04-27 13:23:17 -0400

    Working landing page in React

--- fb/client/src/components/pages/Home/Home.js
+++ fb/client/src/components/pages/Home/Home.js
@@ -1,80 +1,79 @@
 import React, { Component, Fragment } from 'react';
-
+import "./style.css";
 
 function Home() {
   return (
     
-    <Fragment>
-      <p> A Working Thing To Break</p>
+  <Fragment>
 
-      {/* <div className="menu-wrap">
-      <input type="checkbox" className="toggler"/>
-      <div className="hamburger">
-        <div></div>
-      </div>
-      <div className="menu">
+  <div className="menu-wrap">
+    <input type="checkbox" className="toggler"/>
+    <div className="hamburger">
+      <div></div>
+    </div>
+    <div className="menu">
+      <div>
         <div>
-          <div>
-            <ul>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Game</a></li>
-              <li><a href="#">Sign-Up</a></li>
-              <li><a href="#">Logout</a></li>
-            </ul>
-          </div>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Game</a></li>
+            <li><a href="#">Sign-Up</a></li>
+            <li><a href="#">Logout</a></li>
+          </ul>
         </div>
       </div>
     </div>
+  </div>
 
-    <header className="showcase">
-      <div className="container showcase-inner">
-        <h1 className="glow">CROW RUSH</h1>
+  <header className="showcase">
+    <div className="container showcase-inner">
+      <h1 className="glow">CROW RUSH</h1>
         {/* <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas maiores sint impedit delectus quam molestiae
-          explicabo cum facere ratione veritatis.</p> --> 
-        <a href="#" className="btn btn-light">Start Game</a>
-      </div>
-    </header>
+        explicabo cum facere ratione veritatis.</p> -->  */}
+      <a href="#" className="btn btn-light">Start Game</a>
+    </div>
+  </header>
 
-    <div className="container1 backgroundtexture1">
-      <div className="row">
-        <div className="col-12">
-          <h6 className="power">Powered By:</h6>
-        </div>
+  <div className="container1 backgroundtexture1">
+    <div className="row">
+      <div className="col-12">
+        <h6 className="power">Powered By:</h6>
+      </div>
         {/* <!-- <div className="col-sm">
-        </div> --> 
-        <div className="col-12 reactlogo">
-          <img src="assets/images/React.js_logo-512.png" width="500rem" height="500rem" alt="logo for react"/>
-        </div>
+      </div> -->  */}
+      <div className="col-12 reactlogo">
+        <img src="assets/images/React.js_logo-512.png" width="500rem" height="500rem" alt="logo for react"/>
+      </div>
         {/* <!-- <div className="col-sm">
-        </div> --> 
+      </div> -->  */}
+    </div>
+  </div>
+
+  <div className="container2">
+    <div className="row">
+
+      <div className="col-sm">
+        <p>YOUR MISSION, SHOULD YOU CHOOSE TO ACCEPT IT: TWO DAYS LOADED WITH RADICAL LEARNING, GROWING, AND QUALITY
+          TIME WITH THE WONDERFUL LARAVEL COMMUNITY.</p>
       </div>
+
     </div>
+  </div>
 
-    <div className="container2">
-      <div className="row">
+  <div className="slideshow-container">
 
-        <div className="col-sm">
-          <p>YOUR MISSION, SHOULD YOU CHOOSE TO ACCEPT IT: TWO DAYS LOADED WITH RADICAL LEARNING, GROWING, AND QUALITY
-            TIME WITH THE WONDERFUL LARAVEL COMMUNITY.</p>
-        </div>
+    {/* <!-- Full-width images with number and caption text -->  */}
+  <div className="mySlides fade">
+    <div className="numbertext">1 / 3</div>
+      <img src="assets/images/differentbirds.png" style={{'width': '100%'}}/>
+    <div className="text">Caption Text</div>
+  </div>
 
-      </div>
-    </div>
-
-    <div className="slideshow-container">
-
-    {/* <!-- Full-width images with number and caption text --> 
-    <div className="mySlides fade">
-      <div className="numbertext">1 / 3</div>
-        <img src="assets/images/differentbirds.png" style={{width: '100%'}}/>
-      <div className="text">Caption Text</div>
-    </div>
-
-    <div className="mySlides fade">
-      <div className="numbertext">2 / 3</div>
-        <img src="img2.jpg" style={{width: '100%'}}/>
-      <div className="text">Caption Two</div>
-    </div>
+  <div className="mySlides fade">
+    <div className="numbertext">2 / 3</div>
+      <img src="img2.jpg" style={{width: '100%'}}/>
+    <div className="text">Caption Two</div>
+  </div>
 
   <div className="mySlides fade">
     <div className="numbertext">3 / 3</div>
@@ -82,14 +81,14 @@ function Home() {
     <div className="text">Caption Three</div>
   </div>
 
-  {/* <!-- Next and previous buttons --> 
+   {/* <!-- Next and previous buttons -->  */}
   <a className="prev" onclick="plusSlides(-1)">&#10094;</a>
   <a className="next" onclick="plusSlides(1)">&#10095;</a>
   </div>
   <br/>
 
-  {/* <!-- The dots/circles --> 
-  <div style="text-align:center">
+   {/* <!-- The dots/circles -->  */}
+  <div style={{"text-align":"center"}}>
     <span className="dot" onclick="currentSlide(1)"></span>
     <span className="dot" onclick="currentSlide(2)"></span>
     <span className="dot" onclick="currentSlide(3)"></span>
@@ -143,9 +142,8 @@ function Home() {
 
 </div>
 </div> 
-  */}
 
-    </Fragment>
+  </Fragment>
     
   );
 }
