1693d9220216bea1d4975a1b4d780e9a13fa587e
Chlora <ma662@njit.edu>
2019-04-27 12:52:44 -0400

    Add page framework; Add Home Page

---
+++ fb/client/src/components/pages/Home/Home.js
@@ -0,0 +1,153 @@
+import React, { Component, Fragment } from 'react';
+
+
+function Home() {
+  return (
+    
+    <Fragment>
+      <p> A Working Thing To Break</p>
+
+      {/* <div className="menu-wrap">
+      <input type="checkbox" className="toggler"/>
+      <div className="hamburger">
+        <div></div>
+      </div>
+      <div className="menu">
+        <div>
+          <div>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Game</a></li>
+              <li><a href="#">Sign-Up</a></li>
+              <li><a href="#">Logout</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <header className="showcase">
+      <div className="container showcase-inner">
+        <h1 className="glow">CROW RUSH</h1>
+        {/* <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas maiores sint impedit delectus quam molestiae
+          explicabo cum facere ratione veritatis.</p> --> 
+        <a href="#" className="btn btn-light">Start Game</a>
+      </div>
+    </header>
+
+    <div className="container1 backgroundtexture1">
+      <div className="row">
+        <div className="col-12">
+          <h6 className="power">Powered By:</h6>
+        </div>
+        {/* <!-- <div className="col-sm">
+        </div> --> 
+        <div className="col-12 reactlogo">
+          <img src="assets/images/React.js_logo-512.png" width="500rem" height="500rem" alt="logo for react"/>
+        </div>
+        {/* <!-- <div className="col-sm">
+        </div> --> 
+      </div>
+    </div>
+
+    <div className="container2">
+      <div className="row">
+
+        <div className="col-sm">
+          <p>YOUR MISSION, SHOULD YOU CHOOSE TO ACCEPT IT: TWO DAYS LOADED WITH RADICAL LEARNING, GROWING, AND QUALITY
+            TIME WITH THE WONDERFUL LARAVEL COMMUNITY.</p>
+        </div>
+
+      </div>
+    </div>
+
+    <div className="slideshow-container">
+
+    {/* <!-- Full-width images with number and caption text --> 
+    <div className="mySlides fade">
+      <div className="numbertext">1 / 3</div>
+        <img src="assets/images/differentbirds.png" style={{width: '100%'}}/>
+      <div className="text">Caption Text</div>
+    </div>
+
+    <div className="mySlides fade">
+      <div className="numbertext">2 / 3</div>
+        <img src="img2.jpg" style={{width: '100%'}}/>
+      <div className="text">Caption Two</div>
+    </div>
+
+  <div className="mySlides fade">
+    <div className="numbertext">3 / 3</div>
+      <img src="img3.jpg" style={{width: '100%'}}/>
+    <div className="text">Caption Three</div>
+  </div>
+
+  {/* <!-- Next and previous buttons --> 
+  <a className="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a className="next" onclick="plusSlides(1)">&#10095;</a>
+  </div>
+  <br/>
+
+  {/* <!-- The dots/circles --> 
+  <div style="text-align:center">
+    <span className="dot" onclick="currentSlide(1)"></span>
+    <span className="dot" onclick="currentSlide(2)"></span>
+    <span className="dot" onclick="currentSlide(3)"></span>
+  </div>
+
+  <div className="container3">
+    <div className="row">
+      <div className="col-12 designers">
+        <h2 className="designers1"> The Designers</h2>
+      </div>
+
+    <div className="column">
+      <div className="card">
+        <img src="img1.jpg" alt="Jason Sips" style={{width: '100%'}}/>
+        <div className="container">
+          <h2>Jason Sips</h2>
+          <p className="title">Front-End Designer</p>
+          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+          <p>example@example.com</p>
+          <p><button className="button">Portfolio</button></p>
+        </div>
+      </div>
+  </div>
+
+  <div className="column">
+    <div className="card">
+      <img src="img2.jpg" alt="Myles" style={{width: '100%'}}/>
+      <div className="container">
+        <h2>Myles </h2>
+        <p className="title">Back-End Designer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button className="button">Portfolio</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div className="column">
+    <div className="card">
+      <img src="img3.jpg" alt="Tristan Martin" style={{width: '100%'}}/>
+      <div className="container">
+        <h2>Tristan Martin</h2>
+        <p className="title">Front-End Designer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button className="button">Portfolio</button></p>
+      </div>
+    </div>
+  </div>
+
+
+</div>
+</div> 
+  */}
+
+    </Fragment>
+    
+  );
+}
+
+export default Home;
