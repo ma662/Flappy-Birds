ac5723e6fd8a1f0461667053943d917f03d3fcb1
Tristan Martin <smove50@gmail.com>
2019-05-09 13:59:41 -0400

    updating front page again

--- fb/client/src/pages/Login/Login.js
+++ fb/client/src/pages/Login/Login.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import "./style.css";
+import "./login.css";
 
-class LoginPage extends Component { 
+class LoginPage extends Component {
   render() {
     return( 
+      
+      <div className="menu-wrap">
+        <input type="checkbox" className="toggler" />
+        <div className="hamburger">
+          <div></div>
+        </div>
+        <div className="menu">
+          <div>
+            <div>
+              <ul>
+                <li><a href="/home">Home</a></li>
+                <li><a href="/game">Game</a></li>
+                <li><a href="/signup">Sign-Up</a></li>
+                {/* <li><a href="/login">Login</a></li> */}
+                <li><a href="/home">Logout</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
       <div className="box flex " id="log-in">
         <form action="login" method="POST">
           User Name:<br />
@@ -18,7 +40,7 @@ class LoginPage extends Component {
           <br />
             <Link to="/signup">Create Account</Link>
         </form>
-      </div>
+      {/* </div>  */}
     );
   }
 }
