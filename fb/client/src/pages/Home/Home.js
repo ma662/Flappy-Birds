7d23087f30ad10649f5ec2400532ec388ad1c61e
Tristan Martin <smove50@gmail.com>
2019-05-09 12:49:20 -0400

    frontend completed

--- fb/client/src/pages/Home/Home.js
+++ fb/client/src/pages/Home/Home.js
@@ -59,17 +59,22 @@ function Home() {
           </div>
           {/* <!-- <div className="col-sm">
         </div> -->  */}
-          <div className="col-12 reactlogo animated zoomIn delay-1s">
+          <div className="col-12 reactlogo animated zoomIn delay-2s">
             <img src="assets/images/React.js_logo-512.png" alt="react-logo" width="150rem" height="200rem" />
           </div>
 
         </div>
       </div>
 
+
+      <div className="floatinggif">
+      <img src="/assets/images/floatingob.gif" class="size" alt="floating gif" />
+      </div>
+
       <div className="container2">
         <div className="row">
 
-          <div className="col-sm animated fadeInRight delay-2s">
+          <div className="col-sm animated fadeInRight delay-3s">
             <p>YOUR MISSION, SHOULD YOU CHOOSE TO ACCEPT IT: THIS GAME WAS DEVELOP TO TEST YOUR SKILL, PASSION, AND ABLILITY 
             TO TRY AND GET THE TOP SCORE.</p>
           </div>
@@ -93,14 +98,17 @@ function Home() {
         <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
           <div class="carousel-inner">
             <div class="carousel-item active">
-              <img src="/assets/images/backgroundcontent2.jpg" class="size" alt="..." />
+              <img src="/assets/images/gallery3.jpg" class="size" alt="..." />
             </div>
             <div class="carousel-item">
-              <img src="/assets/images/clouds2-1.jpg" class="d-block w-100" alt="..." />
+              <img src="/assets/images/clouds2-1.jpg" class="size" alt="..." />
             </div>
             <div class="carousel-item">
-              {/* <img src="/assets/images/backgroundcontent4.jpg" class="d-block w-100" alt="..." /> */}
+              <img src="/assets/images/gallery5.jpg" class="size" alt="..." />
             </div>
+            {/* <div class="carousel-item">
+              <img src="/assets/images/gallery5.jpg" class="size" alt="..." />
+            </div> */}
           </div>
           <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -118,7 +126,7 @@ function Home() {
       {/* testing out bird design since parallax dont work */}
 
       <div class="testbird">
-      <img class="animated fadeInLeft delay-2s" src="assets/images/crowforfrontend.png" alt="react-logo"/>
+      <img class="animated fadeInLeft delay-8s" src="assets/images/crowforfrontend.png" alt="react-logo"/>
       </div>
 
 
@@ -126,43 +134,43 @@ function Home() {
       <div className="container3">
         <div className="row">
           <div className="col-12 designers">
-            <h2 className="designers1 animated heartBeat delay-3s"> The Designers</h2>
+            <h2 className="designers1 animated heartBeat delay-10s"> The Designers</h2>
             <p class="laser">WE'RE LASER FOCUSED ON IMMEDIATELY ACTIONABLE CONTENT FROM A WIDE RANGE OF GREAT DESIGNERS</p>
           </div>
 
-          <div className="column">
+          <div className="column animated flipInY delay-11s">
             <div className="card">
               <img src="/assets/images/jason.jpg" alt="Jason" style={{ width: '100%' }} />
               <div className="container">
                 <h2>Jason</h2>
                 <p className="title">Front-End Designer</p>
-                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                <p>example@example.com</p>
+                {/* <p>Some text that describes me lorem ipsum ipsum lorem.</p> */}
+                <p>JasonCipollari@gmail.com</p>
                 <p><a className="portfolio" href="https://jasonsips.github.io/Portfolio/"><button className="button1">Portfolio</button></a></p>
               </div>
             </div>
           </div>
 
-          <div className="column">
+          <div className="column animated flipInY delay-12s">
             <div className="card">
               <img src="/assets/images/myles.jpg" alt="Myles" style={{ width: '100%' }} />
               <div className="container">
                 <h2>Myles</h2>
                 <p className="title">Back-End Designer</p>
-                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                <p>example@example.com</p>
-                <p><a className="portfolio" href="https://smove50.github.io/portfolio/"><button className="button1">Portfolio</button></a></p>
+                {/* <p>Some text that describes me lorem ipsum ipsum lorem.</p> */}
+                <p>myles.alcala@yahoo.com</p>
+                <p><a className="portfolio" href="http://github.com/ma662"><button className="button1">Portfolio</button></a></p>
               </div>
             </div>
           </div>
 
-          <div className="column">
+          <div className="column animated flipInY delay-20s">
             <div className="card">
               <img src="/assets/images/tristan.jpg" alt="Tristan" style={{ width: '100%' }} />
               <div className="container">
                 <h2>Tristan</h2>
                 <p className="title">Front-End Designer</p>
-                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                {/* <p>Some text that describes me lorem ipsum ipsum lorem.</p> */}
                 <p>Tristanmartin88@gmail.com</p>
                 <p><a className="portfolio" href="https://smove50.github.io/portfolio/"><button className="button1">Portfolio</button></a></p>
               </div>
