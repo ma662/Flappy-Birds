24bab2e7dccf792f7d31679b2c1c7677a1b9b949
Chlora <ma662@njit.edu>
2019-05-09 16:44:18 -0400

    New favicon, Modify BurgerMenu

--- fb/client/src/components/burger-menu/BurgerMenu.js
+++ fb/client/src/components/burger-menu/BurgerMenu.js
@@ -4,8 +4,21 @@ import "./style.css";
 
 
 function BurgerMenu() {
-  return (
 
+  
+  // home 
+    // signup / login
+
+  // game
+    // logout / home
+
+  // signup 
+    // home
+  
+  // logout
+    // home
+    
+  return (
     <div className="menu-wrap">
       <input type="checkbox" className="toggler" />
       <div className="hamburger">
@@ -16,10 +29,10 @@ function BurgerMenu() {
           <div>
             <ul>
               <li><a href="/home">Home</a></li>
-              <li><a href="/game">Game</a></li>
+              {/* <li><a href="/game">Game</a></li> */}
               <li><a href="/signup">Sign-Up</a></li>
               <li><a href="/login">Login</a></li>
-              <li><a href="/home">Logout</a></li>
+              <li><a href="/">Logout</a></li>
             </ul>
           </div>
         </div>
